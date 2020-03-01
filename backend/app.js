2837fe1c7d368490d07bbbbf99b258a6371e1e05
MaxDeale <maxericdeale@gmail.com>
2020-03-01 20:37:51 +0200

    ya

--- backend/app.js
+++ backend/app.js
@@ -77,7 +77,11 @@ if(process.env.NODE.ENV === "production"){
   //set static folder 
   app.use(express.static("client/build"));
 
-  app.get("*", (req,res)=> res.sendFile(path.resolve(__dirname,"client", "build", "index.html") ));
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client", "build", "index.html") );
+
+  }); 
+
 }
 
 
